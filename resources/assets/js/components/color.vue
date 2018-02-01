faa8a8305316d74dfa301e28a80d2753696e5068
MaiwennB <maiwenn.bizouarn@etu.univ-nantes.fr>
2018-02-01 23:02:44 +0100

    /

---
+++ resources/assets/js/components/color.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">test</div>
+                    <div class="panel-body">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+	new Vue({
+		el: 'dico',
+		data: {
+			color: ''
+			value :''
+		},
+		ready :function(){
+			this.getColor();
+		},
+		method: {
+			getColor : function() {
+				$.ajax({
+					context: this,
+					url: "http://localhost:8000/api/test",
+				})
+			}
+		}
+
+	});
+</script>script>
